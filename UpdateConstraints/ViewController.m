be329a291dbc0991df3bc4c2b31e2593f694e050
Giuseppe Santaniello <giuseppesantaniello@yahoo.it>
2013-10-26 14:47:01 +0200

    add constraints in UIBuilder

--- UpdateConstraints/ViewController.m
+++ UpdateConstraints/ViewController.m
@@ -10,16 +10,52 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UIView *squareView;
+
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *squareCenterX;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *squareCenterY;
+
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *squareWidth;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *squareHeight;
+
 @end
 
 @implementation ViewController
-
+{
+    NSArray *squarePosition; //of Constraints
+    NSArray *squareDimension; //of Constraints
+}
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+
+    squarePosition = @[self.squareCenterX, self.squareCenterY];
+    squareDimension = @[self.squareWidth, self.squareHeight];
 }
 
+-(void)willRotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration
+{
+    if(UIInterfaceOrientationIsLandscape(toInterfaceOrientation)){
+        [self.squareView removeConstraints:[self.squareView constraints]];
+        [self.view removeConstraints:[self.view constraints]];
+        
+        NSDictionary *myViews = @{@"squareView" : self.squareView};
+        
+        [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[squareView]|" options:0 metrics:nil views:myViews]];
+        [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[squareView]|" options:0 metrics:nil views:myViews]];
+        
+    }
+    
+    if(UIInterfaceOrientationIsPortrait(toInterfaceOrientation)){
+        [self.squareView removeConstraints:[self.squareView constraints]];
+        [self.view removeConstraints:[self.view constraints]];
+        
+        [self.squareView addConstraints:squareDimension];
+        [self.view addConstraints:squarePosition];
+        
+    }
+
+}
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
